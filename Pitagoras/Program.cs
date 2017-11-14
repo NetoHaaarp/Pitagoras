9b99ae0d708900065ed5c9353b36d48a00fd1a80
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-13 22:31:11 -0600

    Agregar archivos de proyecto.

---
+++ Pitagoras/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace prog
+{
+    class Pitagoras
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Teorema de pitagoras");
+            Console.WriteLine("Inserte a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte b");
+            double b = double.Parse(Console.ReadLine());
+            a = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine("h = " + a);
+
+            Console.ReadKey();
+        }
+
+    }
+}
